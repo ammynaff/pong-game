00aaa9a58184c7543cd3d15f645fe54e190373e6
Amy Naff <ammynaff@charter.net>
2018-12-14 12:05:57 -0800

    second commit

---
+++ pong.js
@@ -0,0 +1,24 @@
+class Vec {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect {
+    constructor(w, h) {
+        this.pos = new Vec;
+        this.size = new Vec(w, h);
+    }
+}
+
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
+//draws black background
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+//draws the white ball/square
+context.fillStyle = '#fff';
+context.fillRect(0, 0, 10, 10);
